a7a0ad3a862bfb108e6313f9e2cb0dba72eab8bb
Andrew Ross <andrew@vermonster.com>
2015-12-04 15:33:39 -0500

    concurrency for DecisionForest
    note: this increases the runtime of the test from 0.07s to 0.17s. We'll see later if the overhead of spawning multiple processes is amortized across a larger dataset.

--- decision_forest.py
+++ decision_forest.py
@@ -1,18 +1,31 @@
 from collections import Counter
+from axis_aligned_decision_tree import AxisAlignedDecisionTree
+import multiprocessing as mp
+
+def grow_tree(tree_class, dataset, n_points):
+    return tree_class(dataset.bootstrap(n_points))
 
 class DecisionForest:
-    def __init__(self, dataset, tree_class, tree_count, point_count=None):
-        self.trees = [tree_class(dataset.bootstrap(point_count)) for i in range(tree_count)]
+    def __init__(self, dataset, tree_class=None, n_trees=None, n_points=None, processor=None, processes=None):
+        if n_trees is None: n_trees = 50 # TODO -- how many?
+        if n_points is None: n_points = len(dataset.points)
+        if processes is None: processes = 10 # TODO -- how many?
+        if processor is None: processor = mp.Pool(processes=processes)
+        if tree_class is None: tree_class = AxisAlignedDecisionTree
+
+        self.trees = [processor.apply(grow_tree, (tree_class, dataset, n_points)) for _i in range(n_trees)]
+
+    def vote_on(self, point):
+        # TODO: we could return early as soon as we have a definite plurality
+        return Counter(tree.classify(point) for tree in self.trees)
 
     def classify(self, point):
-        outcomes = Counter(tree.classify(point) for tree in self.trees)
-        return outcomes.most_common(1)[0][0]
+        return self.vote_on(point).most_common(1)[0][0]
 
 if __name__ == '__main__':
     import unittest
     from dataset import Dataset
     from list_datapoint import ListDatapoint
-    from axis_aligned_decision_tree import AxisAlignedDecisionTree
 
     class TestDecisionForest(unittest.TestCase):
         def test_classification(self):
@@ -22,11 +35,11 @@ if __name__ == '__main__':
             point4 = ListDatapoint([1, 'T'])
             point5 = ListDatapoint([1, 'T'])
             dataset = Dataset([point1, point2, point3, point4, point5])
-            forest = DecisionForest(dataset, AxisAlignedDecisionTree, 500, 5)
+            forest = DecisionForest(dataset, n_trees=500, n_points=5)
             self.assertEqual(forest.classify(point1), 'H')
             self.assertEqual(forest.classify(point5), 'T')
             self.assertEqual(forest.classify(point3), 'T')
-            forest = DecisionForest(dataset, AxisAlignedDecisionTree, 5, 500)
+            forest = DecisionForest(dataset, n_trees=5, n_points=500)
             self.assertEqual(forest.classify(point1), 'H')
             self.assertEqual(forest.classify(point5), 'T')
             self.assertEqual(forest.classify(point3), 'T')
