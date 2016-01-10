38a323e95b47983798e0f2fe2246cbe97d765c59
Andrew Ross <andrew@vermonster.com>
2016-01-10 13:38:52 -0500

    also test scikit-learn default implementations
    accuracy is sometimes the same, sometimes better. performance for sklearn is much, much faster.

--- bin/evaluate.py
+++ bin/evaluate.py
@@ -7,8 +7,11 @@ from attribute import *
 from axis_aligned_decision_tree import *
 from pruned_decision_tree import *
 from decision_forest import *
+from classifier import *
 import datetime
 import cProfile
+import sklearn.tree
+import sklearn.ensemble
 
 def aa_decision_tree(dataset):
     return AxisAlignedDecisionTree(dataset, min_samples_split=10, max_depth=100)
@@ -18,6 +21,22 @@ def pruned_aa_decision_tree(dataset):
 
 def parallel_forest(dataset):
     return DecisionForest(dataset, n_processes=1, n_trees=50, tree_class=aa_decision_tree)
+
+class SklearnTree(Classifier):
+    def __init__(self, dataset):
+        self.tree = sklearn.tree.DecisionTreeClassifier(criterion='entropy')
+        self.tree.fit(dataset.X, dataset.y)
+
+    def classify(self, x):
+        return self.tree.predict([x])[0]
+
+class SklearnForest(Classifier):
+    def __init__(self, dataset):
+        self.forest = sklearn.ensemble.RandomForestClassifier(criterion='entropy')
+        self.forest.fit(dataset.X, dataset.y)
+
+    def classify(self, x):
+        return self.forest.predict([x])[0]
 
 def generate_dataset(filename):
     # Convert CSV to numpy arrays of X and y
@@ -65,5 +84,5 @@ for dataset_file in dataset_files:
     if dataset_file.startswith('letter'): continue
     print("*******************\n"+dataset_file+"\n*******************")
     dataset = generate_dataset(os.path.join(dataset_path, dataset_file))
-    compare([aa_decision_tree, pruned_aa_decision_tree, parallel_forest], dataset)
+    compare([aa_decision_tree, pruned_aa_decision_tree, SklearnTree, SklearnForest, parallel_forest], dataset)
     print()
