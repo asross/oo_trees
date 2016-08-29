8cbdc7a90474abbeddcfccdaadea0e67d67aec86
Andrew Ross <andrew@vermonster.com>
2016-08-29 12:13:24 -0400

    slight refactors

--- oo_trees/canonical_correlation_splitter_finder.py
+++ oo_trees/canonical_correlation_splitter_finder.py
@@ -2,7 +2,6 @@ import numpy
 import random
 from .splitter import *
 from sklearn.cross_decomposition import CCA
-from numpy.linalg import matrix_rank
 
 class LinearCombination():
     def __init__(self, attributes, weights):
@@ -17,19 +16,17 @@ class CanonicalCorrelationSplitterFinder():
         if n is None:
             n = int(numpy.ceil(numpy.sqrt(len(dataset.attributes))))
 
-        if n != len(dataset.attributes):
-            self.attributes = random.sample(dataset.attributes, n)
-        else:
-            self.attributes = dataset.attributes
-
         self.dataset = dataset
+        self.attributes = random.sample(dataset.attributes, n)
 
-        X = dataset.X.take([a.index for a in self.attributes], 1)
-        Y = dataset.y
         cca = CCA(n_components=1, tol=tol)
-        cca.fit(X, Y)
-        weights = cca.x_weights_.transpose()[0]
-        self.linear_combination = LinearCombination(self.attributes, weights)
+        cca.fit(
+            dataset.X.take([a.index for a in self.attributes], 1),
+            dataset.y)
+
+        self.linear_combination = LinearCombination(
+            self.attributes,
+            cca.x_weights_.transpose()[0])
 
     def each_splitter(self):
         values = numpy.array([self.linear_combination.of(x) for x in self.dataset.X])
